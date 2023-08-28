e29887ab66b488ecae4817e4aa597793e98fee29
Alexpc18 <alexanderpc18@hotmail.com>
2023-08-28 00:20:17 -0700

    fixing script for program 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,29 +1,13 @@
 #include "main.h"
-/**
- * mul - multiply 2 int
- * @a: first int
- * @b: second int
- * Return: addition of the multiplicattion
- */
-
-int mul(int a, int b)
-{
-	return (a * b);
-}
-
-
-2-main.c
-#include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul - multiplies two int
+ * @x: The first number to be checked
+ * @y: The second number to be checked
+ * Return: Multiplication of integers
  */
-int main(void)
+
+int mul(int x, int y)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+	return (x * y);
 }
