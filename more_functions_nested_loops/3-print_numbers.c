2a6b8c3624649a4bf69d9bb10b98008e98459af4
Alexpc18 <alexanderpc18@hotmail.com>
2023-08-28 00:22:36 -0700

    fix script 3-print_numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,33 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
- * return: void
+ * print_numbers - Displays numbers 0 to 9 on a new line
+ * Return: The number from 0 to 9
  */
 
 void print_numbers(void)
 {
-	char c;
+	int a = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	while (a <= 9)
 	{
-		_putchar(c);
+	_putchar(a + '0');
+	a++;
 	}
+
 	_putchar('\n');
-}
-
-
-3-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-    print_numbers();
-    return (0);
 }
